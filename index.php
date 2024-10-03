cccf9ff416c1292e08e350ce89c8907931a1eaa5
GiorgioDordo <djole6789@gmail.com>
2024-10-03 17:07:21 +0200

    Stampati i dati senza tabella

--- index.php
+++ index.php
@@ -1,3 +1,47 @@
+<?php
+
+$hotels = [
+
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+
+];
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,7 +52,23 @@
 </head>
 
 <body>
+    <main>
 
+        <article>
+            <?php foreach ($hotels as $hotel) { ?>
+            <ul>
+                <li>
+                    <p><?= $hotel["name"]; ?></p>
+                    <p><?= $hotel["description"]; ?></p>
+                    <p><?= $hotel["parking"]  ? "si" : "no"; ?></p>
+                    <p><?= $hotel["vote"]; ?></p>
+                    <p><?= $hotel["distance_to_center"]; ?></p>
+                </li>
+            </ul>
+            <?php } ?>
+        </article>
+
+    </main>
 </body>
 
 </html>
